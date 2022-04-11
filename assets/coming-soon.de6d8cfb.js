21f662516928f75bd30eecb54a09b1d8c7375e0d
Asif Mahmud Shimon <ams.eee09@gmail.com>
2022-04-11 16:44:10 +0000

    [skip ci] Updates

---
+++ assets/coming-soon.de6d8cfb.js
@@ -0,0 +1,1 @@
+import{z as o}from"./vendor.fc8bb6e6.js";import{j as i,F as e,a as n}from"./index.fa288ba3.js";function a(){return i(e,{children:[n(o,{children:n("title",{children:"Asif Mahmud Shimon | Coming Soon"})}),n("div",{className:"coming-soon",children:i("p",{children:["Coming soon",n("span",{children:"."}),n("span",{children:"."}),n("span",{children:"."})]})})]})}export{a as default};
